c94f2c51b0ba3b6222dc6a261eed58efcdd2ff28
valenzandoli <63553922+valenzandoli@users.noreply.github.com>
2022-03-24 09:03:20 -0300

    Translation

--- hangman.py
+++ hangman.py
@@ -1,12 +1,12 @@
-# Codigo para juego del Ahorcado, proyecto final del curso de Python intermedio de Platzi.
+# Hangman game code for the Intermediate Python Platzi's course.
 
 import random
 import os
 
-# Abrimos y leemos el archivo 
+# Opening and reading the file
 def read():
     words = []
-    with open("/Users/valenzandoli/Desktop/Data Science/Python intermedio/code/data.txt", "r", encoding="utf-8") as f:
+    with open("./data.txt", "r", encoding="utf-8") as f:
         for word in f:
             words.append(word.strip().upper())
     return words
@@ -17,7 +17,7 @@ def run():
     letters = [x for x in word]
     empty_letters = ["_" for x in letters]
     letter_index_dict = {}
-    # El enumerate te da las letras y un numero para cada una de las letras que la contiene
+    # enumerate gives you the letters and a number for each letter that contains it
     for idx, letter in enumerate(word):
         if not letter_index_dict.get(letter):
             letter_index_dict[letter] = []
@@ -25,13 +25,13 @@ def run():
 
     while True:
         os.system("clear")
-        print("Adivina la palabra ")
+        print("Guess the word! ")
         for element in empty_letters:
             print(element +" ", end = "")
         print("\n")
 
-        letter = input("Ingresa una letra: ").strip().upper() # Con strip se le sacan los espacios en blanco y upper la hace mayuscula a la string
-        assert letter.isalpha(), "Solo puedes ingresar letras" # El assert se asegura que solo se ingresen letras 
+        letter = input("Enter a letter: ").strip().upper() # strip takes the gaps out, upper makes every letter uppercase
+        assert letter.isalpha(), "You can only enter letters" # The assert statement makes sure that you only enter letters 
 
         if letter in letters:
             for idx in letter_index_dict[letter]:
@@ -39,7 +39,7 @@ def run():
         
         if "_" not in empty_letters:
             os.system("clear")
-            print("Ganaste, la palabra era", word)
+            print("Congratulations! You won, the word was: ", word)
             break
 
 
